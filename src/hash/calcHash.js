3f90e3a712bd9a10d2ac6929262b737efc561478
phillomella <121259223+phillomella@users.noreply.github.com>
2025-04-24 18:36:09 +0300

    Hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import crypto from "crypto";
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const filePath = path.join(
+          __dirname,
+          "files",
+          "fileToCalculateHashFor.txt"
+        );
+    
+        const hash = crypto.createHash("sha256");
+        const fileBuffer = await fs.readFile(filePath);
+    
+        hash.update(fileBuffer);
+        const result = hash.digest("hex");
+    
+        console.log(`SHA256 Hash: ${result}`);
+      } catch (err) {
+        console.error(`Error reading file: ${err.message}`);
+      }
 };
 
 await calculateHash();
