3d425ac46772eadca7f8b8f80a08bdf334fa2e2a
phillomella <121259223+phillomella@users.noreply.github.com>
2025-04-24 19:59:11 +0300

    child processes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,21 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+import { spawn } from "child_process";
+import { fileURLToPath } from "url";
+import path from "path";
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function spawnChildProcess(args) {
+  const scriptPath = path.join(__dirname, "./files/script.js");
+  const child = spawn("node", [scriptPath, ...args]);
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
+  child.stderr.pipe(process.stderr);
+
+  child.on("close", (code) => {
+    console.log(`Child process exited with code ${code}`);
+  });
+}
+
+spawnChildProcess(process.argv.slice(2));
