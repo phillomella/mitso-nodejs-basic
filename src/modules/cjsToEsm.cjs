a26bdeaec8fd0033a57373e508f5581bbde4bca6
phillomella <121259223+phillomella@users.noreply.github.com>
2025-04-24 18:25:49 +0300

    Modules

--- src/modules/cjsToEsm.cjs
+++ src/modules/cjsToEsm.cjs
@@ -1,27 +1,33 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from "path";
+import { fileURLToPath } from "url";
+import { release, version } from "os";
+import { createServer as createServerHttp } from "http";
+import { createRequire } from "module";
+import "./files/c.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const require = createRequire(import.meta.url);
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+  unknownObject = require(path.join(__dirname, "files/a.json"));
 } else {
-    unknownObject = require('./files/b.json');
+  unknownObject = require(path.join(__dirname, "files/b.json"));
 }
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
 console.log(`Path segment separator is "${path.sep}"`);
-
 console.log(`Path to current file is ${__filename}`);
 console.log(`Path to current directory is ${__dirname}`);
 
 const myServer = createServerHttp((_, res) => {
-    res.end('Request accepted');
+  res.end("Request accepted");
 });
 
 const PORT = 3000;
@@ -29,12 +35,8 @@ const PORT = 3000;
 console.log(unknownObject);
 
 myServer.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-    console.log('To terminate it, use Ctrl+C combination');
+  console.log(`Server is listening on port ${PORT}`);
+  console.log("To terminate it, use Ctrl+C combination");
 });
 
-module.exports = {
-    unknownObject,
-    myServer,
-};
-
+export { unknownObject, myServer };
