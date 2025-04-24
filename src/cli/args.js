42cb0f515e7a9bd475a5b89606fa1b791d89c2e5
phillomella <121259223+phillomella@users.noreply.github.com>
2025-04-24 18:08:47 +0300

    Command line interface

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    const result = {};
+  
+    for (let i = 0; i < args.length; i += 2) {
+      const propName = args[i].substring(2);
+      const value = args[i + 1];
+      result[propName] = value;
+    }
+  
+    for (const [propName, value] of Object.entries(result)) {
+      console.log(`${propName} is ${value}`);
+    }
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envVars = process.env;
+  
+    const mitsoVars = Object.entries(envVars)
+      .filter(([key]) => key.startsWith("MITSO_"))
+      .map(([key, value]) => `${key}=${value}`)
+      .join("; ");
+  
+    console.log(mitsoVars);
 };
 
 parseEnv();
